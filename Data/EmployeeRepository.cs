57e2182ca0e3a2d5c466a2d3ef5278473418b2d4
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-08-05 15:45:10 -0400

    couple left

---
+++ Data/EmployeeRepository.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DepartmentsEmployees.Models;
+using System.Data.SqlClient;
+
+namespace DepartmentsEmployees.Data
+{
+    class EmployeeRepository
+    {
+        public SqlConnection Connection
+        {
+            get
+            {
+                // This is "address" of the database
+                string _connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=DepartmentsEmployees;Integrated Security=True";
+                return new SqlConnection(_connectionString);
+            }
+        }
+        public List<Employee> GetEmployees()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, FirstName, LastName, DepartmentId FROM Employee";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Employee> employees = new List<Employee>();
+
+                    while (reader.Read())
+                    {
+                        int idColumnPosition = reader.GetOrdinal("Id");
+
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int firstNameColumnPosition = reader.GetOrdinal("FirstName");
+                        int lastNameColumnPosition = reader.GetOrdinal("LastName");
+                        int deptIdColumnPosition = reader.GetOrdinal("DepartmentId");
+                        string firstNameValue = reader.GetString(firstNameColumnPosition);
+                        string lastNameValue = reader.GetString(lastNameColumnPosition);
+                        int deptIdValue = reader.GetInt32(deptIdColumnPosition);
+
+                        Employee employee = new Employee
+                        {
+                            Id = idValue,
+                            FirstName = firstNameValue,
+                            LastName = lastNameValue,
+                            DepartmentId = deptIdValue
+                        };
+
+                        employees.Add(employee);
+                    }
+                    reader.Close();
+                    return employees;
+                }
+            }
+        }
+        public Employee GetEmployeeById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT FirstName, LastName, DepartmentId FROM Employee WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Employee employee = null;
+
+                    if (reader.Read())
+                    {
+                        employee = new Employee
+                        {
+                            Id = id,
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId"))
+                        };
+                    }
+
+                    reader.Close();
+
+                    return employee;
+                }
+            }
+        }
+        public void AddEmployee(Employee employee)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO Department (FirstName, LastName, DeptId) OUTPUT INSERTED.Id Values (@firstName, @lastName, @deptId)";
+                    cmd.Parameters.Add(new SqlParameter("@firstName", employee.FirstName));
+                    cmd.Parameters.Add(new SqlParameter("@lastName", employee.LastName));
+                    cmd.Parameters.Add(new SqlParameter("@deptId", employee.DepartmentId));
+                    int id = (int)cmd.ExecuteScalar();
+                    employee.Id = id;
+                }
+            }
+        }
+        public void UpdateEmployee(int id, Employee employee)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE Employee
+                                     SET FirstName = @firstName, LastName = @lastName
+                                     WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@firstName", employee.FirstName));
+                    cmd.Parameters.Add(new SqlParameter("@firstName", employee.LastName));
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public void DeleteEmployee(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Employee WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        //COPY AND PASTE ABOVE THIS LINE
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using DepartmentsEmployees.Data;
+using DepartmentsEmployees.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DepartmentsEmployees
+{
+    class Program
+    //Control F5 to run app
+    //F5 debugger
+    {
+        static void Main(string[] args)
+        {
+
+            DepartmentRepository departmentRepo = new DepartmentRepository();
+
+            Console.WriteLine("Getting All Departments:");
+            Console.WriteLine();
+
+            List<Department> allDepartments = departmentRepo.GetAllDepartments();
+
+            foreach (Department dept in allDepartments)
+            {
+                Console.WriteLine($"{dept.Id} {dept.DeptName}");
+            }
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Getting Department with Id 1");
+
+            Department singleDepartment = departmentRepo.GetDepartmentById(1);
+
+            Console.WriteLine($"{singleDepartment.Id} {singleDepartment.DeptName}");
+
+
+
+
+            //Department legalDept = new Department
+            //{
+            //    DeptName = "Legal"
+            //};
+
+            //departmentRepo.AddDepartment(legalDept);
+
+            //Console.WriteLine("-------------------------------");
+            //Console.WriteLine("Added the new Legal Department!");
+
+            //departmentRepo.UpdateDepartment(1, new Department() { DeptName = "New Department" });
+
+            //Console.WriteLine("Here's your updated department:");
+            //Console.WriteLine(departmentRepo.GetDepartmentById(1).DeptName);
+
+            //Console.WriteLine("Now let's delete a department.");
+            //departmentRepo.DeleteDepartment(4);
+            //List<Department> newDeps = departmentRepo.GetAllDepartments();
+            //foreach (Department dep in newDeps)
+            //{
+            //    Console.WriteLine(dep.DeptName);
+            //}
+
+            //Console.ReadLine();
+
+            EmployeeRepository employeeRepo = new EmployeeRepository();
+
+            Console.WriteLine("Getting all Employees:");
+            Console.WriteLine();
+
+
+            List<Employee> allEmployees = employeeRepo.GetEmployees();
+            foreach (Employee empl in allEmployees)
+            {
+                Console.WriteLine(empl.FirstName);
+                Console.WriteLine(empl.LastName);
+            }
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Getting Employees with Id 1");
+
+            Employee singleEmployee = employeeRepo.GetEmployeeById(1);
+
+            Console.WriteLine($"{singleEmployee.Id} {singleEmployee.FirstName} {singleEmployee.LastName}");
+
+
+
+
+            //public List<Employee> GetAllEmployeesWithDepartment() < ---Employee objects should have a Department property
+            //public void AddEmployee(Employee employee)
+            //public void UpdateEmployee(int id, Employee employee) < ---Use the SQL UPDATE statement
+            //public void DeleteEmployee(int id)
+            //COPY AND PASTE ABOVE THIS LINE
+        }
+    }
+}
